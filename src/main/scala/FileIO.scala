e5e79940829620b1079384e7f737b1957ba8d453
Emil12212 <emil.morano@mi.unc.edu.ar>
2026-03-22 14:59:36 -0300

    ejercicio 2

--- src/main/scala/FileIO.scala
+++ src/main/scala/FileIO.scala
@@ -1,29 +1,19 @@
 import scala.io.Source
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
+
 
 object FileIO {
+
+
   type Subscription = (String, String)
 
-  def readSubscriptions(path: String): Option[List[Subscription]] = {
-    try {
-      val source = Source.fromFile(path)
-      val content = source.mkString
-      source.close()
 
-      implicit val formats: DefaultFormats.type = DefaultFormats
-      val json = parse(content)
-
-      val subscriptions = json.children.map { sub =>
-        val name = (sub \ "name").extract[String]
-        val url = (sub \ "url").extract[String]
-        (name, url)
-      }
-      Some(subscriptions)
-    } catch {
-      case _: Exception => None
-    }
+  def readSubscriptions(): List[Subscription] = {
+    List(
+      ("scala", "https://www.reddit.com/r/scala/.json?count=10"),
+      ("learnprogramming", "https://www.reddit.com/r/learnprogramming/.json?count=10")
+    )
   }
+
 
   def downloadFeed(url: String): Option[String] = {
     try {
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,35 +1,17 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    val header = s"Reddit Post Parser\n${"=" * 40}"
-    println(header)
+import PostParser._
 
-    val subscriptionsOpt = FileIO.readSubscriptions("subscriptions.json")
+object Main extends App {
 
-    val allPosts: List[(String, String)] = subscriptionsOpt match {
-      case Some(subscriptions: List[(String, String)]) =>
-        subscriptions.flatMap { case (name, url) =>
-          println(s"Fetching posts from: $name")
+  val subscriptions: List[FileIO.Subscription] = FileIO.readSubscriptions()
 
-          FileIO.downloadFeed(url) match {
-            case Some(posts) =>
-              Some((name, posts))
-            case None =>
-              println(s"Error descargando feed: $name")
-              None
-          }
-        }
 
-      case None =>
-        println("Error leyendo suscripciones o JSON mal formado")
-        List()
-    }
-
-    val output = allPosts
-      .map { case (name, posts) =>
-        Formatters.formatSubscription(name, posts)
+  val allPosts: List[Post] = subscriptions.flatMap {
+    case (name, url) =>
+      FileIO.downloadFeed(url) match {
+        case Some(json) => PostParser.parsePosts(json, name)
+        case None => List()
       }
-      .mkString("\n")
-
-    println(output)
   }
+
+  allPosts.foreach(println)
 }
